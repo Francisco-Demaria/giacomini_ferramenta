38ac9f5c1be10a88a62d570d04d83bebc76e3653
Francisco Demaria <demariafranciscodemelo@gmail.com>
2026-08-27 20:56:59 +0000

    tentativa de precos

--- js/core/precos.js
+++ js/core/precos.js
@@ -35,7 +35,7 @@ function calcularPrecos(produto) {
     return {
         precoVista: custo,
         precoParcelado: valorEspecial,
-        precoDe: precoVista * 1.7,
+        precoDe: custo * 1.7,
         possuiPrecoEspecial: true
     };
 }
